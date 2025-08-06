d1f8cf006c4c800230e2441fa854f1f249e8738b
imran9512 <48657619+imran9512@users.noreply.github.com>
2025-08-06 16:32:01 +0500

    4.4p25

--- src/pages/admin/all.jsx
+++ src/pages/admin/all.jsx
@@ -1,9 +1,7 @@
 // src/pages/admin/all.jsx
-import dynamic from 'next/dynamic';
-
-// Render the React admin **only** on the client
-const AdminClient = dynamic(() => import('../../components/AdminClient'), {
-  ssr: false,
-});
-
-export default AdminClient;
+export default function AdminPage() {
+  if (typeof window !== 'undefined') {
+    window.location.replace('/admin/index.html');
+  }
+  return null;
+}
