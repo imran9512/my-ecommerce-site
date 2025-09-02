89851ce2272ad33f0a229cfffab4cb02010d64a8
imran9512 <48657619+imran9512@users.noreply.github.com>
2025-09-02 19:17:13 +0500

    /-start

--- src/utils/redirectsData.mjs
+++ src/utils/redirectsData.mjs
@@ -1,13 +1,13 @@
 // redirectsData.js
 const productRedirects = [
     {
-        source: 'products/vyas-30mg-lisdexamfetamine-as-vyvanse-original-price-in-pakistan',
-        destination: 'products/vyas-30mg-lisdexamfetamine-as-vyvanse-price-in-pakistan',
+        source: '/products/vyas-30mg-lisdexamfetamine-as-vyvanse-original-price-in-pakistan',
+        destination: '/products/vyas-30mg-lisdexamfetamine-as-vyvanse-price-in-pakistan',
         permanent: true,
     },
     {
-        source: 'products/vyas-70mg-lisdexamfetamine-as-vyvanse-original-price-in-pakistan',
-        destination: 'products/vyas-70mg-lisdexamfetamine-as-vyvanse-available-in-pakistan',
+        source: '/products/vyas-70mg-lisdexamfetamine-as-vyvanse-original-price-in-pakistan',
+        destination: '/products/vyas-70mg-lisdexamfetamine-as-vyvanse-available-in-pakistan',
         permanent: true,
     }
     // add new redirects here..
