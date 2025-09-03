e49e8c114a2a21676b7f2960dfb7756925f3c438
imran9512 <48657619+imran9512@users.noreply.github.com>
2025-09-03 17:00:21 +0500

    links updated

--- src/utils/redirectsData.mjs
+++ src/utils/redirectsData.mjs
@@ -9,6 +9,16 @@ const productRedirects = [
         source: '/products/vyas-70mg-lisdexamfetamine-as-vyvanse-original-price-in-pakistan',
         destination: '/products/vyas-70mg-lisdexamfetamine-as-vyvanse-available-in-pakistan',
         permanent: true,
+    },
+    {
+        source: '/help',
+        destination: '/faq',
+        permanent: true,
+    },
+    {
+        source: '/category/delay/timing',
+        destination: '/category/delay-timing',
+        permanent: true,
     }
     // add new redirects here.
 ];
