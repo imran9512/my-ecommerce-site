6c8b31bc2266d78aa1b2bc0c6167ea329dd8e244
imran9512 <48657619+imran9512@users.noreply.github.com>
2025-10-20 14:53:11 +0500

    footer mb-12

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -3,7 +3,7 @@ import { SITE_NAME } from '@/data/constants';
 
 const PageFooter = () => {
     return (
-        <footer className="mb-10 py-2">
+        <footer className="mb-12 py-2">
             <div className="max-w-7xl mx-auto px-4 text-center">
                 <p className="text-sm text-gray-800">Secure & Discreet Delivery</p>
                 <p className="text-sm text-gray-800">&copy; 2025 {SITE_NAME}. All rights reserved.</p>
