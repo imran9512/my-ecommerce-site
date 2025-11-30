a6a8ac10570ecc247d4adcae90c1d40ea4ab1b6c
imran9512 <48657619+imran9512@users.noreply.github.com>
2025-11-30 18:27:42 +0500

    practisa %

--- src/data/products.js
+++ src/data/products.js
@@ -242,7 +242,7 @@ const products = [
       "ATTV70"],
     stripPrice: "1790",
     stripQtyDiscount: [
-      { from: 2, to: 10, start: 4, end: 20 },
+      { from: 2, to: 8, start: 4, end: 20 },
     ],
     stripImage: "/api/images/ritalin/practisa/treatment-of-adhd-in-pakistan.webp",
     stripStock: true,
