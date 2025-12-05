948ec9bc497b9594742732dfa5494fbb3e8aacb0
imran9512 <48657619+imran9512@users.noreply.github.com>
2025-12-06 02:55:05 +0500

    analytics code added in document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,6 +13,18 @@ class MyDocument extends Document {
             name="google-site-verification"
             content={GOOGLE_SEARCH_CONSOLE_TAG.match(/content="([^"]+)"/)[1]}
           />
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BJ9QK4H14S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BJ9QK4H14S');
+</script>
+
         </Head>
         <body>
           <Main />
