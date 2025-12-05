dee9e03eeca08c5654b53a5a2f0aee2756d74aaa
imran9512 <48657619+imran9512@users.noreply.github.com>
2025-12-06 02:40:30 +0500

    analytics code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,6 +17,16 @@ function MyApp({ Component, pageProps }) {
       <Head>
         {/* Google Search Console tag */}
         <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION} />
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BJ9QK4H14S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BJ9QK4H14S');
+</script>
+
       </Head>
 
       <Layout>
